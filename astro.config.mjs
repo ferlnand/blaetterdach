b8c93cd7ab5cbe9ea5ae394caf91ef10f0ba3f4a
Ferdinand <f.legerer@gmail.com>
2025-04-27 23:29:19 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ferlnand.github.io',
+  base: '/blaetterdach',
   build: {
     assets: 'assets'
   }
